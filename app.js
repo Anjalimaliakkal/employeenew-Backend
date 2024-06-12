78399c33213d7527744d9e3b20bbe92280048eb0
Anjalimaliakkal <anjalimsanjali2@gmail.com>
2024-06-12 23:03:10 +0530

    app second commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const employee = require("./models/employee")
+const { employeesmodel } = require("./models/employee")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://anjali2003:anjali2003@cluster0.wy6js.mongodb.net/employeenewdb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/", (req, res) => {
+    let input = req.body
+    console.log(input)
+    let employee = new employeesmodel(input)
+    employee.save()
+    res.json({ "status": "success" })
+})
+
+app.listen(8081, () => {
+    console.log("server started")
+})
